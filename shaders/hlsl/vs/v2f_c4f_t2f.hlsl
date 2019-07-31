7be7ad517f7782b0a11a7224507cbef86265aacf
Adam Simmons <hi@adamjsimmons.com>
2019-07-31 14:23:58 -0500

    Update HLSL vertex shaders with new transformation pipeline (orthographic projection is now applied on CPU).

--- shaders/hlsl/vs/v2f_c4f_t2f.hlsl
+++ shaders/hlsl/vs/v2f_c4f_t2f.hlsl
@@ -13,12 +13,6 @@ float ScreenWidth() { return State[1]; }
 float ScreenHeight() { return State[2]; }
 float Scalar(int i) { if (i < 4) return Scalar4[0][i]; else return Scalar4[1][i - 4]; }
 
-float2 ScreenToDeviceCoords(float2 screen_coord) {
-  screen_coord *= 2.0 / float2(ScreenWidth(), -ScreenHeight());
-  screen_coord += float2(-1.0, 1.0);
-  return screen_coord;
-}
-
 struct VS_OUTPUT
 {
   float4 Position    : SV_POSITION;
@@ -31,8 +25,7 @@ VS_OUTPUT VS(float2 Position : POSITION,
              float2 ObjCoord : TEXCOORD0)
 {
   VS_OUTPUT output;
-  float2 screen_coord = mul(Transform, float4(Position, 1.0, 1.0)).xy;
-  output.Position = float4(ScreenToDeviceCoords(screen_coord), 1.0, 1.0);
+  output.Position = mul(Transform, float4(Position, 0.0, 1.0));
   output.Color = float4(Color) / 255.0;
   output.ObjectCoord = ObjCoord;
   return output;
--- shaders/hlsl/vs/v2f_c4f_t2f_t2f_d28f.hlsl
+++ shaders/hlsl/vs/v2f_c4f_t2f_t2f_d28f.hlsl
@@ -13,12 +13,6 @@ float ScreenWidth() { return State[1]; }
 float ScreenHeight() { return State[2]; }
 float Scalar(int i) { if (i < 4) return Scalar4[0][i]; else return Scalar4[1][i - 4]; }
 
-float2 ScreenToDeviceCoords(float2 screen_coord) {
-  screen_coord *= 2.0 / float2(ScreenWidth(), -ScreenHeight());
-  screen_coord += float2(-1.0, 1.0);
-  return screen_coord;
-}
-
 struct VS_OUTPUT
 {
   float4 Position    : SV_POSITION;
@@ -40,7 +34,7 @@ VS_OUTPUT VS(float2 Position : POSITION,
              float2 TexCoord : TEXCOORD0,
              float2 ObjCoord : TEXCOORD1,
              float4 Data0    : COLOR1,
-			       float4 Data1    : COLOR2,
+			 float4 Data1    : COLOR2,
              float4 Data2    : COLOR3,
              float4 Data3    : COLOR4,
              float4 Data4    : COLOR5,
@@ -49,8 +43,7 @@ VS_OUTPUT VS(float2 Position : POSITION,
 {
   VS_OUTPUT output;
   output.ObjectCoord = ObjCoord;
-  output.ScreenCoord = mul(Transform, float4(Position, 1.0, 1.0)).xy;
-  output.Position = float4(ScreenToDeviceCoords(output.ScreenCoord), 1.0, 1.0);
+  output.Position = mul(Transform, float4(Position, 0.0, 1.0));
   output.Color = float4(Color) / 255.0;
   output.TexCoord = TexCoord;
   output.Data0 = Data0;
