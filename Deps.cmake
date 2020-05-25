294c622a78eac70c3919227b3055c8525b2a716f
Adam Simmons <hi@adamjsimmons.com>
2020-05-25 11:10:05 -0500

    Update dependencies.

--- Deps.cmake
+++ Deps.cmake
@@ -12,9 +12,9 @@ elseif (PORT MATCHES "UltralightWin")
     set(PLATFORM "win")
 endif ()
 
-set(ULTRALIGHTCORE_REV "66cf9e25")
-set(WEBCORE_REV "7d0301e8")
-set(ULTRALIGHT_REV "953cc42b")
+set(ULTRALIGHTCORE_REV "f1a1d115")
+set(WEBCORE_REV "fa2213b0")
+set(ULTRALIGHT_REV "174e30ab")
 
 set(ULTRALIGHTCORE_DIR "${CMAKE_SOURCE_DIR}/deps/UltralightCore/")
 set(WEBCORE_DIR "${CMAKE_SOURCE_DIR}/deps/WebCore/")
