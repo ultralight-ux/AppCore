8e926ca02a6f62c84cb9ca04664835e605487595
Adam Simmons <hi@adamjsimmons.com>
2020-06-26 14:07:33 -0500

    Update dependencies (add new CPU renderer backend).

--- Deps.cmake
+++ Deps.cmake
@@ -12,9 +12,9 @@ elseif (PORT MATCHES "UltralightWin")
     set(PLATFORM "win")
 endif ()
 
-set(ULTRALIGHTCORE_REV "274cc603")
-set(WEBCORE_REV "66d3df8d")
-set(ULTRALIGHT_REV "de8fd515")
+set(ULTRALIGHTCORE_REV "a6bcdc9b")
+set(WEBCORE_REV "b66ef607")
+set(ULTRALIGHT_REV "aeb6c7e7")
 
 set(ULTRALIGHTCORE_DIR "${CMAKE_SOURCE_DIR}/deps/UltralightCore/")
 set(WEBCORE_DIR "${CMAKE_SOURCE_DIR}/deps/WebCore/")
